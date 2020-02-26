7c9fbd1a0405a3d26bfc340f73378f01753fd131
k-romero <imkevinromero@gmail.com>
2020-02-25 20:10:57 -0500

    filled out a few methods in phonebook

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,32 +1,39 @@
 package com.zipcodewilmington.phonebook;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBook {
-    HashMap<String, List<String>> phoneMap;
+    Map<String, List<String>> phoneMap;
+    List<String> phoneNumbers;
 
-    public PhoneBook(Map<String, List<String>> phoneMap) {
-        this.phoneMap = new HashMap<>();
-    }
 
     public PhoneBook() {
-        this.phoneMap = new HashMap<>();
+        this.phoneMap = new LinkedHashMap<>();
+        this.phoneNumbers = new ArrayList<>();
     }
 
+    public PhoneBook(Map<String, List<String>> phoneMap) {
+        this.phoneMap = phoneMap;
+    }
+
+
     public void add(String name, String phoneNumber) {
-        phoneMap.put(name, Collections.singletonList(phoneNumber));
+        this.phoneNumbers.add(phoneNumber);
+        this.phoneMap.put(name, this.phoneNumbers);
     }
 
     public void addAll(String name, String... phoneNumbers) {
+        for (int i = 0; i < phoneNumbers.length; i++) {
+            this.phoneMap.put(name, Arrays.asList(phoneNumbers));
+        }
+
     }
 
     public void remove(String name) {
+        this.phoneMap.remove(name);
     }
 
     public Boolean hasEntry(String name) {
